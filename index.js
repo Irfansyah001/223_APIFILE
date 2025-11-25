3dba51758d31cc907f0e0fa50d7dab40dc1d3ab6
Irfansyah001 <syahirfan545@gmail.com>
2025-11-25 22:52:49 +0700

    fix: update import path for apiRoutes in index.js and correct node_module version in package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = process.env.PORT || 3000;

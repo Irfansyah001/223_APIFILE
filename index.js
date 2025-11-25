6829782c465583bec0dad6afc630d88d157dd722
Irfansyah001 <syahirfan545@gmail.com>
2025-11-25 22:40:18 +0700

    feat: add API routes integration in the main server file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
+const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', apiRoutes);
 
 async function startServer() {
     await connectDatabase();
